c37b6f1884c850f4219c8c155acf8c62be892a28
AQL_23 <ale23200@gmail.com>
2025-07-21 20:53:20 -0600

    nuevas pantallas

---
+++ MineriaDatos/src/visualizacion/PaginaRegresion.py
@@ -0,0 +1,18 @@
+
+"""
+Clase: PaginaKmeans
+
+Objetivo: Clase que mantiene la pagina para visualizacion del K medias
+
+Cambios:
+
+    1. Creacion de la clase y cascarazon visual, aquesada 21-07-2025
+"""
+import streamlit as st
+
+class PaginaRegresion():
+    def __init__(self):
+        pass
+
+    def render(self):
+        st.title("📈 Regresión")
